7b2120877e532710e5d6ebbbcc95f43e22e2cd96
renorari <renorari@renorari.net>
2024-02-18 22:15:34 +0900

    ✨  音声読み上げ完成

--- index.ts
+++ index.ts
@@ -1,11 +1,31 @@
 /* VoiceJP Main Script */
-import { REST, Routes, Client, IntentsBitField, SlashCommandBuilder, Colors, ActivityType, PermissionFlagsBits, SlashCommandSubcommandBuilder, SlashCommandStringOption } from "discord.js";
+import fs from "node:fs";
+import path from "node:path";
+import { StreamType, createAudioPlayer, createAudioResource, joinVoiceChannel } from "@discordjs/voice";
+import { REST, Routes, Client, IntentsBitField, SlashCommandBuilder, Colors, ActivityType, PermissionFlagsBits, SlashCommandSubcommandBuilder, SlashCommandStringOption, SlashCommandChannelOption, ChannelType, Message, ChatInputCommandInteraction } from "discord.js";
 import dotenv from "dotenv";
+import { generateVoice } from "./speech";
 dotenv.config();
 
+if (!fs.existsSync(path.join(__dirname, "temp"))) {
+    fs.mkdirSync(path.join(__dirname, "temp"));
+} else {
+    fs.readdirSync(path.join(__dirname, "temp")).forEach((file) => {
+        fs.unlinkSync(path.join(__dirname, "temp", file));
+    });
+}
+
+const voiceModels = JSON.parse(fs.readFileSync(path.join(__dirname, "voice_models/models.json"), "utf-8"));
 const token = process.env.TOKEN as string;
-const client = new Client({ intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.GuildVoiceStates] });
+const client = new Client({ intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildVoiceStates,
+    IntentsBitField.Flags.MessageContent
+]});
 const restClient = new REST({ version: "10" }).setToken(token);
+
+const voiceChannels = new Map();
 
 function setActivity() {
     client.user?.setActivity({
@@ -27,6 +47,14 @@ client.on("ready", async () => {
                     .setName("join")
                     .setDescription("Joins the voice channel.")
                     .setDescriptionLocalization("ja", "ボイスチャンネルに参加します。")
+                    .addChannelOption(
+                        new SlashCommandChannelOption()
+                            .setName("channel")
+                            .setDescription("The channel to join.")
+                            .setDescriptionLocalization("ja", "参加するチャンネル。")
+                            .addChannelTypes(ChannelType.GuildVoice)
+                            .setRequired(true)
+                    )
                     .setDMPermission(false)
                     .setDefaultMemberPermissions(PermissionFlagsBits.Connect),
                 new SlashCommandBuilder()
@@ -49,6 +77,7 @@ client.on("ready", async () => {
                                     .setName("voice-id")
                                     .setDescription("set voice id.")
                                     .setDescriptionLocalization("ja", "音声IDを設定します。")
+                                    .setChoices(...voiceModels.map((voiceModel: { id: string; name: string; }) => ({value: voiceModel.id, name: voiceModel.name})))
                             )
                             .addStringOption(
                                 new SlashCommandStringOption()
@@ -93,16 +122,200 @@ client.on("ready", async () => {
 
 client.on("interactionCreate", async interaction => {
     if (!interaction.isCommand()) return;
-    if (interaction.commandName === "ping") {
+    if (!interaction.member) {
+        await interaction.reply("申し訳ございませんが、DMでは使用できません。");
+        return;
+    }
+    let channel;
+    let connection;
+    let player;
+    let voiceModel: { id: string; file: string; name: string; };
+    let onMessageCreate;
+    switch (interaction.commandName) {
+    case "ping":
         await interaction.reply({
-            content: "Pong!",
+            content: "ポン!",
             embeds: [{
-                title: "Pong!",
-                description: `Latency is ${Date.now() - interaction.createdTimestamp}ms.\nAPI Latency is ${Math.round(client.ws.ping)}ms.`,
+                title: "ポン!",
+                description: `レイテンシ: ${client.ws.ping}m秒`,
                 color: Colors.LuminousVividPink
             }]
         });
+        break;
+    case "join":
+        channel = await interaction.guild?.channels.fetch(interaction.options.get("channel")?.value as string);
+        if (!channel) {
+            await interaction.reply({
+                "content": "エラーが発生しました。",
+                "embeds": [{
+                    "title": "エラー",
+                    "description": "チャンネルが見つかりません。",
+                    "color": Colors.Red
+                }],
+                "ephemeral": true
+            });
+            return;
+        }
+        if (channel.type !== ChannelType.GuildVoice) {
+            await interaction.reply({
+                "content": "エラーが発生しました。",
+                "embeds": [{
+                    "title": "エラー",
+                    "description": "ボイスチャンネルを指定してください。",
+                    "color": Colors.Red
+                }],
+                "ephemeral": true
+            });
+            return;
+        } else if (!channel.joinable) {
+            await interaction.reply({
+                "content": "エラーが発生しました。",
+                "embeds": [{
+                    "title": "エラー",
+                    "description": "参加できません。",
+                    "color": Colors.Red
+                }],
+                "ephemeral": true
+            });
+            return;
+        }
+        if (voiceChannels.has(interaction.guildId as string)) {
+            voiceChannels.get(interaction.guildId as string).connection.destroy();
+            voiceChannels.delete(interaction.guildId as string);
+        }
+        connection = joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+            selfDeaf: false,
+            selfMute: false
+        });
+        player = createAudioPlayer();
+        connection.subscribe(player);
+        voiceChannels.set(interaction.guildId as string, { connection, player });
+        await interaction.reply({
+            "content": "ボイスチャンネルに参加しました。",
+            "embeds": [{
+                "title": "参加",
+                "description": `${channel.name}に参加しました。`,
+                "color": Colors.Green
+            }]
+        });
+        break;
+    case "leave":
+        if (!voiceChannels.has(interaction.guildId as string)) {
+            await interaction.reply({
+                "content": "エラーが発生しました。",
+                "embeds": [{
+                    "title": "エラー",
+                    "description": "ボイスチャンネルに参加していません。",
+                    "color": Colors.Red
+                }],
+                "ephemeral": true
+            });
+            return;
+        }
+        voiceChannels.get(interaction.guildId as string).connection.destroy();
+        voiceChannels.delete(interaction.guildId as string);
+        await interaction.reply({
+            "content": "ボイスチャンネルから退出しました。",
+            "embeds": [{
+                "title": "退出",
+                "description": "ボイスチャンネルから退出しました。",
+                "color": Colors.Green
+            }]
+        });
+        break;
+    case "speech":
+        if (!voiceChannels.has(interaction.guildId as string)) {
+            await interaction.reply({
+                "content": "エラーが発生しました。",
+                "embeds": [{
+                    "title": "エラー",
+                    "description": "ボイスチャンネルに参加していません。",
+                    "color": Colors.Red
+                }],
+                "ephemeral": true
+            });
+            return;
+        }
+        switch ((interaction as ChatInputCommandInteraction).options.getSubcommand()) {
+        case "synthesis":
+            if (voiceChannels.get(interaction.guildId as string).synthesis) {
+                client.off("messageCreate", voiceChannels.get(interaction.guildId as string).synthesis.messageCreate);
+                voiceChannels.get(interaction.guildId as string).synthesis = null;
+                await interaction.reply({
+                    "content": "音声合成を解除しました。",
+                    "embeds": [{
+                        "title": "音声合成",
+                        "description": "音声合成を解除しました。",
+                        "color": Colors.Green
+                    }]
+                });
+                return;
+            }
+            voiceModel = voiceModels.find((voiceModel: { id: string; }) => voiceModel.id === (interaction.options.get("voice-id")?.value ?? "voicea"));
+            if (!voiceModel) {
+                await interaction.reply({
+                    "content": "エラーが発生しました。",
+                    "embeds": [{
+                        "title": "エラー",
+                        "description": "音声IDが見つかりません。",
+                        "color": Colors.Red
+                    }],
+                    "ephemeral": true
+                });
+                return;
+            }
+            onMessageCreate = async (message: Message) => {
+                if (message.author.bot) return;
+                if (message.channel.type !== ChannelType.GuildText) return;
+                if (message.channel.id !== voiceChannels.get(interaction.guildId as string).synthesis.channel) return;
+                if (message.content.length > 100 || message.content == "") return;
+                const filePath = path.join(__dirname, "temp", `${message.id}`);
+                const generatedVoice = await generateVoice(
+                    // eslint-disable-next-line no-useless-escape
+                    `${message.member?.displayName ? message.member.displayName : message.author.username}。${message.cleanContent.replace(/https?:\/\/[\w/:%#\$&\?\(\)~\.=\+\-]+/g, "URL省略")}`,
+                    filePath,
+                    path.join(__dirname, "voice_models", voiceModel.file),
+                    voiceChannels.get(interaction.guildId as string).synthesis.speed,
+                    voiceChannels.get(interaction.guildId as string).synthesis.tone,
+                    voiceChannels.get(interaction.guildId as string).synthesis.intonation,
+                    voiceChannels.get(interaction.guildId as string).synthesis.volume,
+                    0.25
+                ).catch((error) => {
+                    console.error(error);
+                });
+                if (!generatedVoice) return;
+                const resource = createAudioResource(generatedVoice, { inputType: StreamType.Arbitrary });
+                voiceChannels.get(interaction.guildId as string).player.play(resource);
+            };
+            voiceChannels.get(interaction.guildId as string).synthesis = {
+                "channel": interaction.channel?.id as string,
+                "voiceModel": voiceModel,
+                "speed": interaction.options.get("speed")?.value as number ?? 1.25,
+                "tone": interaction.options.get("tone")?.value as number ?? 1,
+                "intonation": interaction.options.get("intonation")?.value as number ?? 1,
+                "volume": interaction.options.get("volume")?.value as number ?? -6,
+                "messageCreate": onMessageCreate
+            };
+            client.on("messageCreate", onMessageCreate);
+            await interaction.reply({
+                "content": "音声合成を設定しました。",
+                "embeds": [{
+                    "title": "音声合成",
+                    "description": `音声: ${voiceModel.name}\n速度: ${voiceChannels.get(interaction.guildId as string).synthesis.speed}\n音程: ${voiceChannels.get(interaction.guildId as string).synthesis.tone}\n抑揚: ${voiceChannels.get(interaction.guildId as string).synthesis.intonation}\n音量: ${voiceChannels.get(interaction.guildId as string).synthesis.volume}`,
+                    "color": Colors.Green
+                }]
+            });
+            break;
+        }
+        break;
     }
 });
 
 client.login(token);
+
+process.on("unhandledRejection", (error) => {
+    console.error("Unhandled promise rejection:", error);
+});
---
+++ speech.ts
@@ -0,0 +1,105 @@
+/* Voice API */
+import path from "node:path";
+import fs from "node:fs";
+import childProcess from "node:child_process";
+const osDicMap: Record<string, string> = {
+    win32: "C:/open_jtalk/dic",
+    darwin: "/opt/homebrew/opt/open-jtalk/dic",
+    linux: "/var/lib/mecab/dic/open-jtalk/naist-jdic"
+};
+const dic = osDicMap[process.platform] ?? osDicMap.linux;
+
+interface VoiceDictionary {
+    [key: string]: string;
+}
+
+const dictionary: VoiceDictionary = fs.readFileSync(path.join(__dirname, "dictionary.dic"), "utf-8")
+    .split("\n")
+    .reduce((obj: VoiceDictionary, line: string) => {
+        const [key, value] = line.split(":");
+        obj[key] = value;
+        return obj;
+    }, {});
+
+/*function englishToKatakana(text: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        fetch("https://www.sljfaq.org/cgi/e2k_ja.cgi?o=json&lang=ja&word=" + encodeURIComponent(text))
+            .then((response) => response.json())
+            .then((data) => {
+                try {
+                    let outputText = text;
+                    for (const wordObj of data.words) {
+                        if (wordObj.j_pron_only) {
+                            const regex = new RegExp("\\b" + wordObj.word + "\\b", "g");
+                            outputText = outputText.replace(regex, wordObj.j_pron_only);
+                        }
+                    }
+                    resolve(outputText);
+                } catch (error) {
+                    resolve(text);
+                }
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}*/
+
+async function generateVoice(text:string, filepath:string, model:string, speed:number, tone:number, intonation:number, volume:number, between:number): Promise<string> {
+    if (!speed) throw new Error("Speed is not defined");
+
+    const processedText = //await englishToKatakana(
+        text.toLowerCase()
+            .replace(new RegExp(Object.keys(dictionary).join("|"), "g"), (match) => dictionary[match]);
+    //);
+
+    const texts: string[] = processedText
+        .trim()
+        .split(/。|\n|\.|,/);
+
+    const promises = texts
+        .filter((n) => n)
+        .map(async (_text, index) => {
+            if (_text === "") return;
+            const filePath = path.join(`${filepath}_${index}.wav`);
+            await execPromise(`echo ${JSON.stringify(_text)} | open_jtalk -x ${dic} -m ${model} -r ${speed} -fm ${tone} -jf ${intonation} -g ${volume} -ow ${filePath}`);
+            return filePath;
+        });
+
+    const voicePaths = (await Promise.all(promises)).filter((n) => n) as string[];
+
+    const blankPath = path.join(`${filepath}_blank_${between}.wav`);
+
+    if (between !== 0) {
+        await execPromise(`ffmpeg -f lavfi -i anullsrc=r=48000:cl=mono -t ${between} ${blankPath}`);
+
+        const voice = voicePaths.map((filePath) => [filePath, blankPath]).flat();
+        const mergedFilePath = path.join(`${filepath}.wav`);
+        await execPromise(`ffmpeg -y ${voice.map((path) => `-i ${path}`).join(" ")} -filter_complex "concat=n=${voice.length}:v=0:a=1" ${mergedFilePath}`);
+        await unlinkPromise(blankPath);
+    } else {
+        const mergedFilePath = path.join(`${filepath}.wav`);
+        await execPromise(`ffmpeg -y ${voicePaths.map((path) => `-i ${path}`).join(" ")} -filter_complex "concat=n=${voicePaths.length}:v=0:a=1" ${mergedFilePath}`);
+    }
+
+    await Promise.all(voicePaths.map((path) => unlinkPromise(path)));
+
+    return `${filepath}.wav`;
+}
+
+function execPromise(command: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        childProcess.exec(command, (error) => {
+            if (error) return reject(error);
+            resolve();
+        });
+    });
+}
+
+function unlinkPromise(filePath: string): Promise<void> {
+    if (!fs.existsSync(filePath)) return Promise.resolve();
+    return fs.promises.unlink(filePath).catch(console.error);
+}
+
+
+export { generateVoice };
