30e574af058dfb15fb466a81d16541c3e7bfc06d
renorari <renorari@renorari.net>
2024-10-24 11:02:39 +0900

    🐛 フォルダの修正

--- includes/folder_manager.ts
+++ includes/folder_manager.ts
@@ -3,10 +3,10 @@
 import fs from "node:fs";
 import path from "node:path";
 
-if (!fs.existsSync(path.join(__dirname, "temp"))) {
-    fs.mkdirSync(path.join(__dirname, "temp"));
+if (!fs.existsSync(path.join(__dirname, "../temp"))) {
+    fs.mkdirSync(path.join(__dirname, "../temp"));
 } else {
-    fs.readdirSync(path.join(__dirname, "temp")).forEach((file) => {
-        fs.unlinkSync(path.join(__dirname, "temp", file));
+    fs.readdirSync(path.join(__dirname, "../temp")).forEach((file) => {
+        fs.unlinkSync(path.join(__dirname, "../temp", file));
     });
 }
