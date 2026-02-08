fec4952b6b1b3e88b64b13111607bcc8e82d3119
renorari <renorari@renorari.net>
2026-02-08 16:25:09 +0900

    🐛 remove: eliminate GuildMembers intent from client options

--- src/discord/constants/index.ts
+++ src/discord/constants/index.ts
@@ -11,7 +11,6 @@ import type { ClientOptions } from "discord.js";
 export const clientOptions: ClientOptions = {
     "intents": [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildVoiceStates,
         GatewayIntentBits.GuildWebhooks,
